43201e04801ff569c2553d4e527887f7cb7c1ab2
igorternyuk <xmonad100.gmail.com>
2017-09-05 13:53:52 +0300

    first commit

---
+++ game.cpp
@@ -0,0 +1,206 @@
+#include "game.h"
+#include <iostream>
+//#include <ctime>
+
+Game::Game():
+    window_(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE)
+{
+    font_.loadFromFile(PATH_TO_FONT);
+    textureCards_.loadFromFile(PATH_TO_IMAGE);
+    textureCards_.setSmooth(true);
+    spriteCards_.setTexture(textureCards_);
+    fillGameFieldRandomly();
+}
+
+void Game::run()
+{
+    while (window_.isOpen()) {
+        inputPhase();
+        drawingPhase();
+    }
+}
+
+void Game::newGame()
+{
+    numSelectedCards_ = 0;
+    indexSelectedCard1_ = 0;
+    indexSelectedCard2_ = 0;
+    fillGameFieldRandomly();
+    isPlaying_ = true;
+}
+
+void Game::fillGameFieldRandomly()
+{
+    cards_.clear();
+    std::uniform_int_distribution<int> distribution_{0,NUM_CARD_TYPES - 1};
+
+    for(int y{0}; y < FIELD_HEIGHT; ++y)
+    {
+        for(int x{0}; x < FIELD_WIDTH; ++x)
+        {
+            bool isRandomIdOK = false;
+            do
+            {
+                int rndNum = distribution_(mt);
+                 if(countEntries(cards_, rndNum) < NUM_SELECTED_CARDS_MAX)
+                {
+                    Card newCard{rndNum, x, y, false};
+                    isRandomIdOK = true;
+                    cards_.push_back(newCard);
+                }
+            }
+            while(!isRandomIdOK);
+        }
+    }
+}
+
+void Game::inputPhase()
+{
+    sf::Event event;
+    while(window_.pollEvent(event))
+    {
+         if(event.type == sf::Event::Closed)
+         {
+             window_.close();
+         }
+         if(event.type == sf::Event::KeyReleased)
+         {
+             switch(event.key.code)
+             {
+                 case sf::Keyboard::N:
+                     std::cout << "N was pressed" << std::endl;
+                     newGame();
+                     break;
+                 case sf::Keyboard::Escape:
+                 case sf::Keyboard::Q:
+                     window_.close();
+                     break;
+                 default:
+                     break;
+             }
+         }
+         if(event.type == sf::Event::MouseButtonReleased &&
+            event.mouseButton.button == sf::Mouse::Button::Left)
+         {
+             if(isPlaying_)
+             {
+                 int clickedCardX = event.mouseButton.x / FRAME_WIDTH;
+                 int clickedCardY = event.mouseButton.y / FRAME_HEIGHT;
+                 //std::cout << "clickedCardX = " << clickedCardX << std::endl;
+                 //std::cout << "clickedCardY = " << clickedCardY << std::endl;
+                 for(unsigned short int i{0}; i < cards_.size(); ++i)
+                 {
+                     if(cards_[i].x == clickedCardX &&
+                        cards_[i].y == clickedCardY)
+                     {
+                         switch(numSelectedCards_)
+                         {
+                             case 0:
+                                 ++numSelectedCards_;
+                                 cards_[i].isShown = true;
+                                 indexSelectedCard1_ = i;
+                                 break;
+                             case 1:
+                                 if(i != indexSelectedCard1_)
+                                 {
+                                     ++numSelectedCards_;
+                                     cards_[i].isShown = true;
+
+                                     indexSelectedCard2_ = i;
+                                     if(cards_[indexSelectedCard1_].id ==
+                                        cards_[indexSelectedCard2_].id)
+                                     {
+                                         numSelectedCards_ = 0;
+                                         std::cout << "Match!" << std::endl;
+                                     }
+                                 }
+                                 else
+                                 {
+                                     std::cout << "Same card was can not be choosen1!" << std::endl;
+                                 }
+                                 break;
+                             case 2:
+                                 if(i != indexSelectedCard1_ && i != indexSelectedCard2_)
+                                 {
+                                     numSelectedCards_ = 0;
+                                     cards_[indexSelectedCard1_].isShown = false;
+                                     cards_[indexSelectedCard2_].isShown = false;
+                                     std::cout << "Don't Match" << std::endl;
+                                 }
+                                 else
+                                 {
+                                     std::cout << "Same card was can not be choosen2!" << std::endl;
+                                 }
+                                 break;
+                             default:
+                                 break;
+                         }
+                         break;
+                     }
+                 }
+                 std::cout << "numSelectedCards_ = " << numSelectedCards_ << std::endl;
+             }
+         }
+    }
+}
+
+bool Game::checkWin()
+{
+    for(const auto &card: cards_)
+    {
+        if(card.isShown == false)
+            return false;
+    }
+    isPlaying_ = false;
+    return true;
+}
+
+void Game::drawingPhase()
+{
+    window_.clear();
+
+    if(checkWin())
+    {
+        drawCongratulations();
+    }
+    else
+    {
+        for(const auto &card: cards_)
+        {
+            if(card.isShown)
+            {
+                spriteCards_.setTextureRect(sf::IntRect(card.id * FRAME_WIDTH, 0,
+                                                       FRAME_WIDTH, FRAME_HEIGHT));
+            }
+            else
+            {
+                spriteCards_.setTextureRect(sf::IntRect(CLOSED_CARD_POSITION *
+                                                        FRAME_WIDTH, 0, FRAME_WIDTH,
+                                                        FRAME_HEIGHT));
+            }
+            spriteCards_.setPosition(FRAME_WIDTH * card.x, FRAME_WIDTH * card.y);
+            window_.draw(spriteCards_);
+        }
+    }
+    window_.display();
+}
+
+void Game::drawCongratulations()
+{
+    sf::Text text("Congratulations!", font_, 48);
+    text.setColor(sf::Color::Yellow);
+    text.setPosition((WINDOW_WIDTH - text.getGlobalBounds().width) / 2,
+                     (WINDOW_HEIGHT - text.getGlobalBounds().height) / 2);
+    window_.draw(text);
+}
+
+int Game::countEntries(const std::vector<Card>& cards, int card_id)
+{
+    int counter{0};
+    for(const auto &c: cards)
+    {
+        if(c.id == card_id)
+            ++counter;
+    }
+    return counter;
+}
---
+++ game.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <string>
+#include <vector>
+#include <random>
+
+class Game
+{
+public:
+    explicit Game();
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) = delete;
+    Game(Game&&) = delete;
+    Game& operator=(Game&&) = delete;
+    void run();
+
+private:
+    const std::string WINDOW_TITLE{"Karten"};
+    const std::string PATH_TO_IMAGE{"resources/gfx/set500.png"};
+    const std::string PATH_TO_FONT{"resources/fonts/orbitron-light.ttf"};
+    enum
+    {
+        FIELD_WIDTH = 6,
+        FIELD_HEIGHT = 4,
+        FRAME_WIDTH = 80,
+        FRAME_HEIGHT = 80,
+        WINDOW_WIDTH = FIELD_WIDTH * FRAME_WIDTH,
+        WINDOW_HEIGHT = FIELD_HEIGHT * FRAME_HEIGHT,
+        NUM_CARD_TYPES = 12,
+        NUM_SELECTED_CARDS_MAX = 2,
+        CLOSED_CARD_POSITION = 12
+    };
+
+    struct Card
+    {
+        int id;
+        int x, y;
+        bool isShown;
+    };
+
+    sf::RenderWindow window_;
+    sf::Sprite spriteCards_;
+    sf::Texture textureCards_;
+    sf::Font font_;
+    sf::Clock clock_;
+    sf::Time tempClock_;
+    std::vector<Card> cards_;
+    std::random_device rd;
+    std::mt19937 mt{rd()};
+    int numSelectedCards_{0};
+    int indexSelectedCard1_{0},
+        indexSelectedCard2_{0};
+    bool isPlaying_{true};
+    void newGame();
+    void fillGameFieldRandomly();
+    void inputPhase();
+    bool checkWin();
+    void drawingPhase();
+    void drawCongratulations();
+    int countEntries(const std::vector<Card>& cards_, int card_id);
+};
+
